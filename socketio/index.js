2c2bf2f3b974febc7d44f7edfb6c5e6e409caed6
wmin0 <wmin0@hotmail.com>
2018-10-01 21:48:56 +0800

    [socketio] refactor frontent js
    1. handle import namespace prevent function leak
    2. naming
    3. better dom operation

--- socketio/index.js
+++ socketio/index.js
@@ -1,61 +1,49 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-const records = require('./records.js')
+const message = require('./model/message')
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/view/index.html')
-})
-
-app.get('/app.js', (req, res) => {
-    res.sendFile(__dirname + '/view/app.js')
-})
-
-app.get('/utils.js', (req, res) => {
-    res.sendFile(__dirname + '/view/utils.js')
-})
-
-app.get('/style.css', (req, res) => {
-    res.sendFile(__dirname + '/view/style.css')
-})
+app.use(express.static(path.join(__dirname, 'view')))
 
 let onlineCount = 0;
 
 io.on('connection', (socket) => {
-    onlineCount++;
-    console.log("New connection, connected: " + onlineCount)
+    onlineCount++
+    console.log(`New connection, connected: ${onlineCount}`)
+
     io.emit('online', onlineCount)
-    socket.emit('maxRecord', records.getMax())
-    records.get((msgs) => {
-        socket.emit('chatRecord', msgs)
-    })
+    socket.emit('max-record', message.getMax())
+
+    message.get()
+    .then((msgs) => socket.emit('chat-records', msgs))
+    .catch(console.error)
 
     socket.on('disconnect', () => {
-        onlineCount--;
+        onlineCount--
+        console.log(`Close connection, connected: ${onlineCount}`)
         io.emit('online', onlineCount)
-        console.log("Close connection, connected: " + onlineCount)
     })
 
     socket.on('send', (msg) => {
         console.log(msg)
-        if (Object.keys(msg).length < 2)
-            return;
-        records.push(msg)
+        if (Object.keys(msg).length < 2) {
+            return
+        }
+        message.push(msg)
     })
 
-    socket.on('remove-all', ()=> {
+    socket.on('remove-all', () => {
         console.log("Remove all data!!!")
-        records.removeAll(function() {
-            io.emit('msg-clear')
-        })
+        message.removeAll()
+        .then(() => io.emit('msg-clear'))
+        .catch(console.error)
     })
 })
 
-records.on('new_message', (msg) => {
-    io.emit('msg', msg)
-})
+message.on('new-message', (msg) => io.emit('msg', msg))
 
 server.listen(3000, () => {
     console.log("Server started to listen port 3000")
