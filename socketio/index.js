94ca2d0d67566b22fa45526aaacdbfbc0fa36967
Ada Chang <adachang123@gmail.com>
2018-10-01 13:19:41 +0800

    [socketio] Add basic connect/disconnect

--- socketio/index.js
+++ socketio/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.sendFile(__dirname + '/view/index.html')
 })
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log("Hello client")
+    socket.on('disconnect', () => {
+        console.log("Bye~ client")
+    })
+})
+
+server.listen(3000, () => {
     console.log("Server started to listen port 3000")
 })
