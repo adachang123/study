3d51de2471a15267857b79fa544e63eca632e122
Ada Chang <adachang123@gmail.com>
2018-10-05 15:01:53 +0800

    [Rxjs] add drag drag effect, refactor

--- rxjs-dragdrop/index.js
+++ rxjs-dragdrop/index.js
@@ -1,17 +1,45 @@
-console.log('Hi Rx')
+(() => {
+    let Rx = window.Rx
+    const video = document.getElementById('video')
 
-const video = document.getElementById('video')
-const anchor = document.getElementById('anchor')
+    function addFixedEffect() {
+        const anchor = document.getElementById('anchor')
+        const scroll = Rx.Observable.fromEvent(document, 'scroll')
 
-const scroll = Rx.Observable.fromEvent(document, 'scroll')
+        scroll
+            .map(e => anchor.getBoundingClientRect().bottom < 0)
+            .subscribe(scrolledBellow => {
+                if (scrolledBellow) {
+                    video.classList.add('video-fixed')
+                } else {
+                    video.classList.remove('video-fixed')
+                }
+            })
+    }
 
-scroll
-    .map(e => anchor.getBoundingClientRect().bottom < 0)
-    .subscribe(scrolledBellow => {
-        if (scrolledBellow) {
-            video.classList.add('video-fixed')
-        } else {
-            video.classList.remove('video-fixed')
-        }
-        console.log(video.classList)
+    function addDragDropEffect() {
+        const mouseDown = Rx.Observable.fromEvent(video, 'mousedown')
+        const mouseUp = Rx.Observable.fromEvent(document, 'mouseup')
+        const mouseMove = Rx.Observable.fromEvent(document, 'mousemove')
+
+        mouseDown
+            .filter(e => video.classList.contains('video-fixed'))
+            .map(e => mouseMove.takeUntil(mouseUp))
+            .concatAll()
+            .withLatestFrom(mouseDown, (move, down) => {
+                return {
+                    x: move.clientX - down.offsetX,
+                    y: move.clientY - down.offsetY
+                }
+            })
+            .subscribe(pos => {
+                video.style.top = pos.y + 'px'
+                video.style.left = pos.x + 'px'
+            })
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        addFixedEffect()
+        addDragDropEffect()
     })
+})()
